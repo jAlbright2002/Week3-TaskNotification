6b3f3f5e4c01e92c662a0cc9bdcb61d800ff987f
James A <G00379271@atu.ie>
2024-09-30 19:33:22 +0100

    Reworked methods

--- src/main/java/ie/atu/week3tasknotification/NotificationController.java
+++ src/main/java/ie/atu/week3tasknotification/NotificationController.java
@@ -7,10 +7,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NotificationController {
 
-    @GetMapping("/test")
-    public void test() {
-        System.out.println("Hate paddy");
-//        return "aijsbhbsdvbuhsdbujhsdkvb ks";
+    @GetMapping("/updated")
+    public String updatedTask() {
+        System.out.println("Updated");
+        return "Updated Task";
+    }
+
+    @GetMapping("/removed")
+    public String removedTask() {
+        System.out.println("Removed");
+        return "Removed Task";
+    }
+
+    @GetMapping("/added")
+    public String addedTask() {
+        System.out.println("Added");
+        return "Added Task";
     }
 
 }
