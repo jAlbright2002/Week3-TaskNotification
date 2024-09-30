54579e8bf7a63e11958223d72c566acee6f47833
James A <G00379271@atu.ie>
2024-09-30 16:28:26 +0100

    Create Project

---
+++ src/main/java/ie/atu/week3tasknotification/Week3TaskNotificationApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week3tasknotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3TaskNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week3TaskNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/week3tasknotification/Week3TaskNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3tasknotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3TaskNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
